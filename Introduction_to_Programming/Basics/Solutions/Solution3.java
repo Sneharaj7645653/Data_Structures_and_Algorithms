6b05f1f16cceb755d321d2df1af9a49ab46963aa
Sneha Raj <sneharaj7645653@gmail.com>
2025-07-15 13:47:41 +0530

    Update Solution3.java

--- Introduction_to_Programming/Basics/Solutions/Solution3.java
+++ Introduction_to_Programming/Basics/Solutions/Solution3.java
@@ -3,10 +3,6 @@ import java.util.*;
 
 public class Solution3 {
     public static void main(String[] args) {
-        // YOUR CODE GOES HERE
-        // Please take input and print output to standard input/output (stdin/stdout)
-        // DO NOT USE ARGUMENTS FOR INPUTS
-        // E.g. 'Scanner' for input & 'System.out' for output
         System.out.print("Hello World !");
     }
 }
