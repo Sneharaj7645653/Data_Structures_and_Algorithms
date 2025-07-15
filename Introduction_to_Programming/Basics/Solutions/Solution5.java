00c0b5dc38e2c1f32e549e45b661bad276c4fb23
Sneha Raj <sneharaj7645653@gmail.com>
2025-07-15 13:48:06 +0530

    Update Solution5.java

--- Introduction_to_Programming/Basics/Solutions/Solution5.java
+++ Introduction_to_Programming/Basics/Solutions/Solution5.java
@@ -3,10 +3,6 @@ import java.util.*;
 
 public class Solution5 {
     public static void main(String[] args) {
-        // YOUR CODE GOES HERE
-        // Please take input and print output to standard input/output (stdin/stdout)
-        // DO NOT USE ARGUMENTS FOR INPUTS
-        // E.g. 'Scanner' for input & 'System.out' for output
         System.out.println("*****\n****\n***\n**\n*");
     }
 }
