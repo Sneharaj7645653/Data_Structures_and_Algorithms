ef4a7ae0004dcedffb0fe2cce426524704bd3332
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-09-19 22:30:52 +0530

    Solution 2

--- DSA101/Subarrays/Solutions/Subarrays_Solution2.java
+++ DSA101/Subarrays/Solutions/Subarrays_Solution2.java
@@ -1,2 +1,25 @@
+import java.util.*;
+
 public class Subarrays_Solution2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        sc.nextLine(); // consume newline after integer
+
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            int n = s.length();
+            long ans = 0;
+            String vowels = "AEIOUaeiou";
+
+            for (int i = 0; i < n; i++) {
+                if (vowels.indexOf(s.charAt(i)) != -1) {
+                    ans += (n - i);
+                }
+            }
+
+            System.out.println(ans % 10003);
+        }
+    }
 }
